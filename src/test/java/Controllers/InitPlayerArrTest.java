6c75a5e456042513be346bcdfed9f97873d1a5ec
Poire Molle <s224311@dtu.dk>
2022-11-14 10:29:20 +0100

    Adding a test for init of player array

---
+++ src/test/java/Controllers/InitPlayerArrTest.java
@@ -0,0 +1,15 @@
+package Controllers;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class InitPlayerArrTest {
+
+    @Test
+    void init() {
+    int playerCount = 2;
+    int balance = 20;
+
+    }
+}
