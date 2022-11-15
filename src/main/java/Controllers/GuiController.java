7b2532414f8626b23f72b97fd90fb651267ddbb7
NegarOstad <s224283@student.dtu.dk>
2022-11-15 12:36:15 +0100

    Gui constractor is changed and added ownable feature to the ownable fields.

--- src/main/java/Controllers/GuiController.java
+++ src/main/java/Controllers/GuiController.java
@@ -16,9 +16,10 @@ import java.awt.*;
      int diceSum = 0;
 
     String message;
-    int players;
+    int player;
     private String playerName;
     private int balance;
+    GUI_Ownable ownable ;
 
 
 
@@ -58,21 +59,30 @@ import java.awt.*;
 
         gui.getFields()[1].setTitle("Burgerbaren");
         gui.getFields()[1].setBackGroundColor(Color.PINK);
-        gui.getFields()[1].setSubText("M1");
+        //gui.getFields()[1].setSubText("M1");
+        ownable = (GUI_Ownable)fields[1];
+        ownable.setSubText("M1");
+
 
         gui.getFields()[2].setTitle("Pizzahuset");
         gui.getFields()[2].setBackGroundColor(Color.red);
-        gui.getFields()[2].setSubText("M1");
+        //gui.getFields()[2].setSubText("M1");
+        ownable = (GUI_Ownable)fields[2];
+        ownable.setSubText("M1");
 
         gui.getFields()[3].setBackGroundColor(Color.MAGENTA);
 
         gui.getFields()[4].setTitle("Slikbuttiken");
         gui.getFields()[4].setBackGroundColor(Color.ORANGE);
-        gui.getFields()[4].setSubText("M1");
+        //gui.getFields()[4].setSubText("M1");
+        ownable = (GUI_Ownable)fields[4];
+        ownable.setSubText("M1");
 
         gui.getFields()[5].setTitle("Iskiosken");
         gui.getFields()[5].setBackGroundColor(Color.blue);
-        gui.getFields()[5].setSubText("M1");
+        //gui.getFields()[5].setSubText("M1");
+        ownable = (GUI_Ownable)fields[5];
+        ownable.setSubText("M1");
 
         gui.getFields()[6].setTitle("I fengsel");
         gui.getFields()[6].setBackGroundColor(Color.gray);
@@ -80,22 +90,29 @@ import java.awt.*;
 
         gui.getFields()[7].setTitle("Museet");
         gui.getFields()[7].setBackGroundColor(Color.PINK);
-        gui.getFields()[7].setSubText("M2");
+        //gui.getFields()[7].setSubText("M2");
+        ownable = (GUI_Ownable)fields[7];
+        ownable.setSubText("M2");
 
         gui.getFields()[8].setTitle("Biblioteket");
         gui.getFields()[8].setBackGroundColor(Color.orange);
-        gui.getFields()[8].setSubText("M2");
-
+        //gui.getFields()[8].setSubText("M2");
+        ownable = (GUI_Ownable)fields[8];
+        ownable.setSubText("M2");
 
         gui.getFields()[9].setBackGroundColor(Color.magenta);
 
         gui.getFields()[10].setTitle("Skateparken");
         gui.getFields()[10].setBackGroundColor(Color.CYAN);
-        gui.getFields()[10].setSubText("M2");
+        //gui.getFields()[10].setSubText("M2");
+        ownable = (GUI_Ownable)fields[10];
+        ownable.setSubText("M2");
 
         gui.getFields()[11].setTitle("Swimmingpoolen");
         gui.getFields()[11].setBackGroundColor(Color.orange);
-        gui.getFields()[11].setSubText("M2");
+        //gui.getFields()[11].setSubText("M2");
+        ownable = (GUI_Ownable)fields[11];
+        ownable.setSubText("M2");
 
         gui.getFields()[12].setTitle("Parkering");
         gui.getFields()[12].setBackGroundColor(Color.darkGray);
@@ -103,21 +120,29 @@ import java.awt.*;
 
         gui.getFields()[13].setTitle("Spillehallen");
         gui.getFields()[13].setBackGroundColor(Color.orange);
-        gui.getFields()[13].setSubText("M3");
+        //gui.getFields()[13].setSubText("M3");
+        ownable = (GUI_Ownable)fields[13];
+        ownable.setSubText("M3");
 
         gui.getFields()[14].setTitle("Biografen");
         gui.getFields()[14].setBackGroundColor(Color.pink);
-        gui.getFields()[14].setSubText("M3");
+        //gui.getFields()[14].setSubText("M3");
+        ownable = (GUI_Ownable)fields[14];
+        ownable.setSubText("M3");
 
         gui.getFields()[15].setBackGroundColor(Color.magenta);
 
         gui.getFields()[16].setTitle("Legetøjsbutikken");
         gui.getFields()[16].setBackGroundColor(Color.orange);
-        gui.getFields()[16].setSubText("M3");
+        //gui.getFields()[16].setSubText("M3");
+        ownable = (GUI_Ownable)fields[17];
+        ownable.setSubText("M3");
 
         gui.getFields()[17].setTitle("Dyrebutikken");
         gui.getFields()[17].setBackGroundColor(Color.pink);
-        gui.getFields()[17].setSubText("M3");
+        //gui.getFields()[17].setSubText("M3");
+        ownable = (GUI_Ownable)fields[17];
+        ownable.setSubText("M3");
 
         gui.getFields()[18].setTitle("I fengsel");
         gui.getFields()[18].setBackGroundColor(Color.gray);
@@ -125,22 +150,30 @@ import java.awt.*;
 
         gui.getFields()[19].setTitle("Bowlinghallen");
         gui.getFields()[19].setBackGroundColor(Color.blue);
-        gui.getFields()[19].setSubText("M4");
+        //gui.getFields()[19].setSubText("M4");
+        ownable = (GUI_Ownable)fields[19];
+        ownable.setSubText("M4");
 
         gui.getFields()[20].setTitle("Zoologisk Have");
         gui.getFields()[20].setBackGroundColor(Color.PINK);
-        gui.getFields()[20].setSubText("M4");
+        //gui.getFields()[20].setSubText("M4");
+        ownable = (GUI_Ownable)fields[20];
+        ownable.setSubText("M4");
 
 
         gui.getFields()[21].setBackGroundColor(Color.MAGENTA);
 
         gui.getFields()[22].setTitle("Vandlandet");
         gui.getFields()[22].setBackGroundColor(Color.CYAN);
-        gui.getFields()[22].setSubText("M5");
+        //gui.getFields()[22].setSubText("M5");
+        ownable = (GUI_Ownable)fields[22];
+        ownable.setSubText("M5");
 
         gui.getFields()[23].setTitle("Strand");
         gui.getFields()[23].setBackGroundColor(Color.orange);
-        gui.getFields()[23].setSubText("M5");
+        //gui.getFields()[23].setSubText("M5");
+        ownable = (GUI_Ownable)fields[23];
+        ownable.setSubText("M5");
 
         gui.getUserButtonPressed("Press Ok to start!");
         gui.showMessage("Welcome to Monopoly Junior game.");
@@ -185,6 +218,12 @@ import java.awt.*;
      }
      public int getCurrentPosition(){
         return getCurrentPosition();
+     }
+     public int getCurrentPlayer(int player){
+        return player;
+     }
+     public int setBalance(int balance){
+        return balance;
      }
 
      public Die getDie1() {
