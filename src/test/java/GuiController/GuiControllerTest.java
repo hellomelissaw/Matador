fae1876f7253431ff731d3658dd1542b43e8789b
NegarOstad <s224283@student.dtu.dk>
2022-11-19 11:06:46 +0100

    some changes

---
+++ src/test/java/GuiController/GuiControllerTest.java
@@ -0,0 +1,28 @@
+package GuiController;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class GuiControllerTest {
+
+    @Test
+    public void addPlayerOnBoard() {
+    }
+
+    @Test
+    public void move() {
+    }
+
+    @Test
+    public void updateBalance() {
+    }
+
+    @Test
+    public void receiveRent() {
+    }
+
+    @Test
+    public void winner() {
+    }
+}
