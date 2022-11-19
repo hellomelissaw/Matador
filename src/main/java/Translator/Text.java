e2919c8a368244fc01d9262c846c4a1bb5c19284
Melissa <s224311@dtu.dk>
2022-11-19 22:35:49 +0100

    Removing unused methods

--- src/main/java/Translator/Text.java
+++ src/main/java/Translator/Text.java
@@ -6,10 +6,11 @@ import java.io.*;
 public class Text {
     String file;
 
-    int lineCount = 42;
+    int lineCount = 43;
     String[] messages = new String[lineCount];
 
     String[] labels = new String[lineCount];
+
     public Text(String file){
         this.file = file;
         BufferedReader reader;
@@ -43,39 +44,6 @@ public class Text {
         }
         return message;
     }
-
-    /*public void printText(int index) {
-        BufferedReader reader;
-
-        try {
-            reader = new BufferedReader(new java.io.FileReader(file));
-
-            String line = "";
-
-            for(int i = 0 ; i < lineCount ; i++) {
-
-                line = reader.readLine();
-                String[] labelsAndMessages = line.split(",");
-                labels[i] = labelsAndMessages[0];
-                messages[i] = labelsAndMessages[1];
-
-            }
-            System.out.println("First label: " + labels[0]);
-            System.out.println("Second label: " + labels[1]);
-            System.out.println("Third label: " + labels[2]);
-
-            System.out.println("First msg: " + messages[0]);
-            System.out.println("Second msg: " + messages[1]);
-            System.out.println("Third msg: " + messages[2]);
-
-
-        } catch (Exception e) {
-            e.printStackTrace();
-
-        }
-
-    }
-*/
 
 }
 
