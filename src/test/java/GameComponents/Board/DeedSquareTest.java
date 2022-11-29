d0be9cfa23aa563242b22c0da30d3116b52ae48a
Melissa <s224311@dtu.dk>
2022-11-29 16:59:34 +0100

    Updating position of player since landOn method doesn't do it

--- src/test/java/GameComponents/Board/DeedSquareTest.java
+++ src/test/java/GameComponents/Board/DeedSquareTest.java
@@ -2,6 +2,7 @@ package GameComponents.Board;
 
 import Controllers.GuiController;
 import GameComponents.Player;
+import gui_fields.GUI_Player;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -12,25 +13,23 @@ public class DeedSquareTest {
     Player testPlayer2 = new Player("TestPlayer 2");
     DeedSquare testDeedSquare = new DeedSquare("Test Deed 1",5,guiController);
 
+    GUI_Player testGuiPlayer1 = new GUI_Player("TestPlayer 1");
+    GUI_Player testGuiPlayer2 = new GUI_Player("TestPlayer 2");
 
     @Test
-    public void testDeedSquareHasDeed() {
-        assertEquals(true,testDeedSquare.hasDeed());
-    }
-    @Test
-    public void setTestDeedSquareHasDeedFalseafterSellDeed() {
-        testDeedSquare.sellDeed(testPlayer1,1);
-        assertEquals(false,testDeedSquare.hasDeed());
-    }
-    @Test
-    public void getDeedPrice() {
-        assertEquals(5, testDeedSquare.getDeedPrice());
-    }
-    @Test
-    public void getDeedOwner() {
-        testDeedSquare.sellDeed(testPlayer1,1);
+    public void deedOwnerIsTestPlayer1AfterLandingOnIt() {
+        testDeedSquare.setLang("EnglishText");
+        testPlayer1.updatePosition(1);
+        testDeedSquare.landOn(testPlayer1, testGuiPlayer1);
         assertEquals(testPlayer1, testDeedSquare.getDeedOwner());
-        testDeedSquare.sellDeed(testPlayer2,1);
-        assertEquals(testPlayer2, testDeedSquare.getDeedOwner());
+    }
+    @Test
+    public void testPlayer2PaysRentToDeedOwner() {
+        testPlayer2.depositMoney(20);
+        testPlayer1.updatePosition(1);
+        testDeedSquare.landOn(testPlayer1, testGuiPlayer1);
+        testPlayer2.updatePosition(1);
+        testDeedSquare.landOn(testPlayer2, testGuiPlayer2);
+        assertEquals(15, testPlayer2.getCurrentBalance());
     }
 }
