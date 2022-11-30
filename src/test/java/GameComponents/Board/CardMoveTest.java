a639aa8a4c6fa527e8eb2702d9ae1b88f02c5ef2
Melissa <s224311@dtu.dk>
2022-11-30 16:12:39 +0100

    Adding a test for CardMove playCard implementation

--- src/test/java/GameComponents/Board/CardMoveTest.java
+++ src/test/java/GameComponents/Board/CardMoveTest.java
@@ -1,4 +1,31 @@
+package GameComponents.Board;
+
+import Controllers.GuiController;
+import GameComponents.Player;
+import Translator.Text;
+import gui_fields.GUI_Player;
+import org.junit.Test;
+
 import static org.junit.Assert.*;
+
+
+
 public class CardMoveTest {
-  
+    GuiController guiController = new GuiController();
+    Player testPlayer1 = new Player("TestPlayer 1");
+
+   // Square testChanceSquare = new ChanceSquare("Testing Chance", guiController);
+    Text msg = new Text("src/main/java/Translator/EnglishText");
+    ChanceCard testChanceCard = new CardMove("chance1",guiController,24);
+
+    GUI_Player testGuiPlayer1 = new GUI_Player("TestPlayer 1");
+
+    @Test
+    public void playCardMoveToStart() {
+        //testChanceSquare.setLang("EnglishText");
+        testChanceCard.setLang(msg);
+        testChanceCard.playCard(testPlayer1,testGuiPlayer1);
+        assertEquals(0, testPlayer1.getPosition());
+
+    }
 }
