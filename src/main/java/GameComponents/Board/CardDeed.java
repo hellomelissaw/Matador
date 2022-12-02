a1f089522b86dd77010699c3cc6a2d8afba03177
Melissa <s224311@dtu.dk>
2022-12-02 08:53:34 +0100

    Fixing Card deed checking if square of Card color is free

--- src/main/java/GameComponents/Board/CardDeed.java
+++ src/main/java/GameComponents/Board/CardDeed.java
@@ -6,6 +6,10 @@ import gui_fields.GUI_Player;
 public class CardDeed extends ChanceCard {
     private String color1;
     private String color2;
+    Square[] sameColorSquares = {};
+    int squareIndex = 0;
+
+    Square[] board;
 
     public CardDeed(String cardMessage, GuiController guiController, String color1, String color2) {
         super(cardMessage, guiController);
@@ -13,8 +17,89 @@ public class CardDeed extends ChanceCard {
         this.color2 = color2;
 
     }
-    public void playCard(Player currentPlayer, GUI_Player currentGuiPlayer){
-       if (color1 == "cyan" || color1 == "red") {
+
+    public void setBoard(Square[] board) {
+       this.board = board;
+    }
+
+    /*public void getSquaresByColor(Square[] board, String color) {
+        for(int i = 0; i < board.length ; i++){
+            if (board[i].getColor() == color) {
+                sameColorSquares[i] = board[i];
+                squareIndex = i;
+            }
+        }
+    }*/
+
+    public void playCard(Player currentPlayer, GUI_Player currentGuiPlayer) {
+        int currentPos = currentPlayer.getPosition();
+        int newPos;
+        if(color1 == "na") {
+            int skateParkIndex = 10;
+            if(currentPos > skateParkIndex){
+                newPos = board.length - currentPlayer.getPosition() + skateParkIndex;
+
+            } else { newPos = skateParkIndex - currentPos; }
+
+            System.out.println("New position is: " + newPos);
+            currentPlayer.updatePosition(newPos);
+
+
+
+        } else {
+            boolean foundFreeSquare = false;
+            Square freeSquare;
+            while(!foundFreeSquare) { // LOOKS FOR FREE SQUARE
+            for (int i = 0 ; i < board.length ; i++) {
+                foundFreeSquare = ((DeedSquare) board[i]).hasDeed();
+                if (foundFreeSquare && (board[i].getColor() == color1 || board[i].getColor() == color2)) {
+                    freeSquare = board[i];
+                } else { foundFreeSquare = false;}
+            }
+            for (int i = 0 ; i < board.length ; i++) {
+                    if (board[i].getColor() == color1 || board[i].getColor() == color2) {
+                        if (board[i] instanceof DeedSquare) {
+
+                            if (squareIsFree) {
+                                ((DeedSquare) board[i]).setDeedOwner(currentPlayer);
+                                if (currentPos > i) {
+                                    newPos = board.length - currentPlayer.getPosition() + i;
+
+                                } else {
+                                    newPos = board.length - currentPos - (board.length - i);
+                                }
+                                System.out.println("THis is new position: " + newPos);
+                                currentPlayer.updatePosition(newPos);
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+
+
+        /*int newSquareIndex = 0;
+        for (int i = 0; i < board.length; i++) {
+            if(board[i].getColor() == color1) {
+                newSquareIndex = i;
+            }
+        }
+        if (color1 == "cyan" || color1 == "red") {
+
+
+                       if (((DeedSquare) sameColorSquares[i]).hasDeed()) {
+                           sameColorSquares[i].landOn(currentPlayer, currentGuiPlayer);
+                           int price = ((DeedSquare) sameColorSquares[i]).deedPrice;
+                           System.out.println("Price of deed: " + price);
+                           currentPlayer.depositMoney(price);
+                           int currentPosition = currentPlayer.getPosition();
+                           int newPosition = Math.abs(squareIndex - currentPosition);
+                           currentPlayer.updatePosition(newPosition);
+
+                       }
+
 
        } else if(color1 == "lightgrey"){
 
@@ -23,7 +108,15 @@ public class CardDeed extends ChanceCard {
        } else if(color1 == "pink"){
 
        } else if (color1 == "na") {
+           int skateParkIndex = 10;
+           int move = skateParkIndex - currentPlayer.getPosition();
+           System.out.println("Absolute value: " + Math.abs(move));
+           currentPlayer.updatePosition(Math.abs(move));
 
        }
+
+         */
     }
+
+
 }
