550b6d15c8c37c23d62d1709e585486031dd23f2
Melissa <s224311@dtu.dk>
2022-12-06 22:32:19 +0100

    Making the init test as a if statement in init instead of its own method

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -5,11 +5,8 @@ public class Game {
     public static void main(String[] args) {
 
         GameController game = new GameController();
-        //USE initTest() TO AVOID HAVING TO ENTER PLAYER COUNT AND NAMES WHEN RUNNING
-        game.initTest();
 
-        //UNCOMMENT TO WHEN RUNNING PROGRAM NORMALLY AND COMMENT game.initTest();
-        //game.init();
+        game.init();
         game.run();
 
     }
