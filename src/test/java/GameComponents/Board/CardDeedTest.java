b88f17d784b2ae21c7a3f5626753297ba49829c5
Melissa <s224311@dtu.dk>
2023-01-06 19:30:27 +0100

    Setting up players and chance square

--- src/test/java/GameComponents/Board/CardDeedTest.java
+++ src/test/java/GameComponents/Board/CardDeedTest.java
@@ -10,10 +10,17 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class CardDeedTest {
-
+    GuiController guiController = new GuiController();
+    Player[] testPlayers = new Player[2];
 
     public CardDeedTest() {
+        testPlayers[0] = new Player("TestPlayer 1");
+        testPlayers[1] = new Player("TestPlayer 2");
+        ChanceSquare testChanceSquare = new ChanceSquare("Chance Square", guiController, testPlayers);
+    }
 
+    @Test
+    public void chanceCard34GoToClosestShipyard() {
 
     }
 
