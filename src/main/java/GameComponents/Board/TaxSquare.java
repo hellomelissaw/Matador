320884706b932f4e94d7b0b60c02ee0ab4cc1c92
NegarOstad <s224283@student.dtu.dk>
2023-01-06 19:52:02 +0100

    Update BoardInit and TaxSquare methode.

---
+++ src/main/java/GameComponents/Board/TaxSquare.java
@@ -0,0 +1,59 @@
+package GameComponents.Board;
+
+import Controllers.GuiController;
+import GameComponents.Board.Square;
+import GameComponents.Player;
+
+public class TaxSquare extends Square {
+
+    GuiController guiController;
+    int userInput;
+    int balance;
+    int newBalance;
+    Player[] players;
+
+    TaxSquare(String squareName , GuiController guiController , Player[] players) {
+        super(squareName);
+        this.guiController = guiController;
+        this.players = players;
+    }
+
+    @Override
+    public void landOn(Player currentPlayer) {
+        int currentPosition = currentPlayer.getPosition();
+
+        if (currentPosition==4){
+            msg.printText("betaleSka", "na");
+            userInput = guiController.getUserInteger("indkomstSkat");
+            if(userInput == 1) {
+                balance = (currentPlayer.getCurrentBalance());
+                newBalance = (int) (balance/100) * 10 ;
+                currentPlayer.withdrawMoney(newBalance);
+                currentPlayer.getCurrentBalance();
+                msg.printText("newBalance","na");
+                System.out.println(msg.getText("newBalance")+ newBalance);
+            }
+            else {
+                balance = currentPlayer.getCurrentBalance();
+                currentPlayer.withdrawMoney(4000);
+                newBalance = currentPlayer.getCurrentBalance();
+                msg.printText("newBalance","na");
+                System.out.println(msg.getText("newBalance")+ newBalance);
+
+            }
+
+
+
+        }
+        if (currentPosition == 38){
+            balance = currentPlayer.getCurrentBalance();
+            currentPlayer.withdrawMoney(2000);
+            newBalance = currentPlayer.getCurrentBalance();
+            msg.printText("newBalance","na");
+            System.out.println(msg.getText("newBalance")+ newBalance);
+
+        }
+
+    }
+}
+
