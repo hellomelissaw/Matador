9af7a0a79148f48ea4c8df61c5dafbb391a06429
Mirzy12 <s205821@student.dtu.dk>
2023-01-06 12:23:08 +0100

    Tilføjelser og modificering af k16 og k17

--- src/main/java/GameComponents/Board/JailSquare.java
+++ src/main/java/GameComponents/Board/JailSquare.java
@@ -2,9 +2,14 @@ package GameComponents.Board;
 
 import GameComponents.Player;
 import Controllers.GuiController;
-
+import GameComponents.Cup;
+import GameComponents.Die;
+import GameComponents.Player;
 public class JailSquare extends Square{
     GuiController guiController;
+    Die  die1 = new Die();
+    Die die2 = new Die();
+
     public JailSquare(String jailSquare, GuiController guiController) {
         super(jailSquare);
         this.guiController = guiController;
@@ -13,23 +18,35 @@ public class JailSquare extends Square{
 
     public void landOn(Player currentPlayer) {
         int currentPosition = currentPlayer.getPosition();
-        if (currentPosition==18){
-            msg.printText("moveToJail", "na");
+        int d1 = die1.roll();
+        int d2 = die2.roll();
+        int i = 0;
 
+        if(currentPosition==31);
+        msg.printText("moveToJail", "na");
+        currentPlayer.updatePosition(20);
 
-            currentPlayer.withdrawMoney(1);
+        //If the player while in prison rolls the dice of the same value, they get an extra as well as leaving prison
+        while(currentPosition==31);
+        if (d1==d2){
+            System.out.println("Lucky shark, you get to leave jail and get an extra turn!");
+            i = i + 1;
+            System.out.println(msg.getText("movedToSquare") + currentPlayer.getPosition() );
             int currentBalance = currentPlayer.getCurrentBalance();
             System.out.println(msg.getText("newBalance")+ currentBalance);
+            System.out.println();
 
-            currentPlayer.updatePosition(12);
+            //note: husk at skrive at spilleren får ekstra tur
+        //The player has the chance to leave prison over the course of three rounds where they have to roll the dice of the same value at least once or they have to pay 1000 to leave
+        }else{
+            System.out.println("Oh you failed..");
 
-            System.out.println(msg.getText("movedToSquare") + currentPlayer.getPosition() );
 
-        }  else  {
+
+        //If they land on visit jail, the player gets a round where they have a break
+        }else{
             System.out.println(msg.getText("visitJail"));
         }
     }
 
 }
-
-
