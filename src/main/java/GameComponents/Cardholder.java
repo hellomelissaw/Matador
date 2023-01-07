cd702fd8671e9d0c3af8f2e1981eb334b0179531
Melissa <s224311@dtu.dk>
2023-01-07 14:22:45 +0100

    UPdating cardholder to divide deed into Deed_Buildable and Deed_NonBuildable

--- src/main/java/GameComponents/Cardholder.java
+++ src/main/java/GameComponents/Cardholder.java
@@ -1,25 +1,30 @@
 package GameComponents;
 
 import GameComponents.Board.Deed;
+import GameComponents.Board.Deed_Buildable;
+import GameComponents.Board.Deed_NonBuildable;
 
 import java.util.ArrayList;
 
 public class Cardholder {
 
-    ArrayList<Deed> deedCards = new ArrayList<Deed>();
+    ArrayList<Deed_Buildable> deedCards_Buildable = new ArrayList<Deed_Buildable>();
+    ArrayList<Deed_NonBuildable> deedCards_NonBuildable = new ArrayList<Deed_NonBuildable>();
 
-    public void addCard(String cardType, Deed deed) {
-        if(cardType.equals("deed")) {
-            deedCards.add(deed);
-        }
 
+    public void addDeedBuildable(Deed_Buildable deed) {
+        deedCards_Buildable.add(deed);
+    }
+
+    public void addDeedNonBuildable(Deed_NonBuildable deed) {
+        deedCards_NonBuildable.add(deed);
     }
     public boolean getOwnerStatus(String color) {
         int groupSize = 0;
         boolean ownerStatus = false;
         int cardCount = 0;
 
-        for(Deed deed : deedCards) {
+        for(Deed_Buildable deed : deedCards_Buildable) {
             if(deed.getColor().equals(color)) {
                 groupSize = deed.getGroupSize();
                 cardCount++;
@@ -33,19 +38,19 @@ public class Cardholder {
         return ownerStatus;
     }
 
-    public Deed[] getDeedList() {
-        Deed[] deedCardsArray = new Deed[deedCards.size()];
-        deedCardsArray = deedCards.toArray(deedCardsArray);
+    public Deed_NonBuildable[] getDeedList() {
+        Deed_NonBuildable[] deedCardsArray = new  Deed_NonBuildable[deedCards_NonBuildable.size()];
+        deedCardsArray = deedCards_NonBuildable.toArray(deedCardsArray);
         return deedCardsArray;
     }
 
-    public boolean houseCountIsLevel(String color, Deed deed) {
+    public boolean houseCountIsLevel(String color, Deed_Buildable deed) {
         boolean countIsLevel = false;
         int newCount = deed.getHouseCount() + 1;
 
-        for(int i = 0 ; i < deedCards.size() ; i++) {
-            if(deedCards.get(i).getColor().equals(color)) {
-                if ((newCount - deedCards.get(i).getHouseCount()) <= 1) {
+        for(int i = 0 ; i < deedCards_Buildable.size() ; i++) {
+            if(deedCards_Buildable.get(i).getColor().equals(color)) {
+                if ((newCount - deedCards_Buildable.get(i).getHouseCount()) <= 1) {
                     countIsLevel = true;
 
                 } else {
