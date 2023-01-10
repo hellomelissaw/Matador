64565f4873f44e8aa2bef45c8134dbf5f6a45d02
Melissa <s224311@dtu.dk>
2023-01-10 18:04:24 +0100

    Adding a method that setsGroupOwner for test

--- src/main/java/GameComponents/Cardholder.java
+++ src/main/java/GameComponents/Cardholder.java
@@ -11,6 +11,7 @@ public class Cardholder {
     ArrayList<Deed_Buildable> deedCards_Buildable = new ArrayList<Deed_Buildable>();
     ArrayList<Deed_NonBuildable> deedCards_NonBuildable = new ArrayList<Deed_NonBuildable>();
 
+    boolean isOwner = false;
 
     public void addDeedBuildable(Deed_Buildable deed) {
         deedCards_Buildable.add(deed);
@@ -21,7 +22,6 @@ public class Cardholder {
     }
     public boolean getOwnerStatus(String color) {
         int groupSize = 0;
-        boolean ownerStatus = false;
         int cardCount = 0;
 
         for(Deed_Buildable deed : deedCards_Buildable) {
@@ -32,10 +32,15 @@ public class Cardholder {
         }
 
         if (cardCount == groupSize) {
-            ownerStatus = true;
+            isOwner = true;
         }
 
-        return ownerStatus;
+        return isOwner;
+
+    }
+
+    public void setOwnerOfGroup() {
+        isOwner = true;
     }
 
     public Deed_NonBuildable[] getNonBuildable() {
