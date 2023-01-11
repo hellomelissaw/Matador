844be64e2c0c4786d928757fe475216518158891
Melissa <s224311@dtu.dk>
2023-01-11 18:08:14 +0100

    +1 to line count

--- src/main/java/Translator/Text.java
+++ src/main/java/Translator/Text.java
@@ -9,7 +9,7 @@ public class Text {
     String file;
     GuiController guiController;
 
-    int lineCount = 93;
+    int lineCount = 95;
     String[] messages = new String[lineCount];
 
     String[] labels = new String[lineCount];
