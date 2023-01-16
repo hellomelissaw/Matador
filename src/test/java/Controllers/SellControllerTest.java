796c4cda05bbde9d1c2fbcae9825c29943fb2d28
NegarOstad <s224283@student.dtu.dk>
2023-01-16 17:35:04 +0100

    Test Taxsquare and SellController

--- src/test/java/Controllers/SellControllerTest.java
+++ src/test/java/Controllers/SellControllerTest.java
@@ -1,10 +1,10 @@
 package Controllers;
 
+import GameComponents.Bank;
 import GameComponents.Board.Deed_Buildable;
 import GameComponents.Board.Deed_NonBuildable;
 import GameComponents.Player;
 import Translator.Text;
-import gui_fields.GUI_Player;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -13,20 +13,28 @@ public class SellControllerTest {
 
     GuiController guiController = new GuiController();
     Text msg = new Text("src/main/java/Translator/DanskTekst", guiController);
-    SellController sellController = new SellController(guiController,msg);
+    //SellController sellController = new SellController(guiController,msg);
     Player testPlayer = new Player("TestPlayer");
     Player buyerPlayer = new Player("BuyerPlayer");
-    int[] array = {1000,50,250,750,2250,4000,6000};
-    Deed_Buildable deedBuildable = new Deed_Buildable(1200 ,array,"Rødorvrevej",500);
-    GUI_Player testGuiPlayer = new GUI_Player("Test Player");
+    int[] arrayBuildable = {1000,50,250,750,2250,4000,6000};
+    int[] arrayNonBuildable = {3000,100,200};
+    Bank bank = new Bank();
+    Deed_Buildable deedBuildable = new Deed_Buildable(1200 , arrayBuildable,"Rødorvrevej",500);
+    Deed_NonBuildable deedNonBuildable = new Deed_NonBuildable(3000,arrayNonBuildable,"Tuborg Squash");
+    //GUI_Player testGuiPlayer = new GUI_Player("Test Player");
 
 
     @Test
     public void testPlayerTakeNonBuildableDeed() {
 
-        Deed_NonBuildable deed = new Deed_NonBuildable(100,array,"Shipping");
+        Deed_NonBuildable deed = new Deed_NonBuildable(100, arrayBuildable,"Shipping");
         testPlayer.takeNonBuildableDeed(deed);
         assertEquals("Shipping" , testPlayer.getNonBuildableDeeds()[0].getDeedName());
+    }
+    @Test
+    public void testPlayerTakeBuildableDeed(){
+        testPlayer.takeBuildableDeed(deedBuildable);
+        assertEquals("Rødorvrevej",testPlayer.getBuildableDeeds()[0].getDeedName());
     }
     @Test
     public void testGetOwnedFields(){
@@ -37,12 +45,12 @@ public class SellControllerTest {
     }
     @Test
     public void testChangeOwner(){
-
-        testPlayer.depositMoney(2000);
-        buyerPlayer.depositMoney(2000);
+        testPlayer.guiIsOn(false);
+        buyerPlayer.guiIsOn(false);
+        testPlayer.depositMoney(2000,false);
+        buyerPlayer.depositMoney(2000, false);
         assertEquals(2000, testPlayer.getCurrentBalance());
         assertEquals(2000,buyerPlayer.getCurrentBalance());
-
         testPlayer.addToOwnedFields(deedBuildable);
     }
 }
--- src/test/java/GameComponents/Board/TaxSquareTest.java
+++ src/test/java/GameComponents/Board/TaxSquareTest.java
@@ -1,11 +1,28 @@
 package GameComponents.Board;
 
+import GameComponents.Bank;
+import GameComponents.Player;
 import org.junit.Test;
+
+import static org.junit.Assert.*;
 
 public class TaxSquareTest {
 
-    @Test
-    public void betaleIndkomstSkat() {
 
+        Player testPlayer = new Player("testPlayer");
+        Bank bank = new Bank();
+        int withdrawMoney = 0;
+        int balance = 0;
+        @Test
+        public void testPayTenProcentTax() {
+            testPlayer.guiIsOn(false);
+            testPlayer.depositMoney(2000,false);
+            assertEquals(2000,testPlayer.getCurrentBalance());
+
+            balance = testPlayer.getCurrentBalance();
+            withdrawMoney = (int) (balance / 100) * 10;
+            testPlayer.withdrawMoney(withdrawMoney,false);
+            assertEquals(1800,testPlayer.getCurrentBalance());
+        }
     }
-}
+
