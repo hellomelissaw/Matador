6c65e052fdf2b27339fcd5b3555b1abeb9d32117
Melissa <s224311@dtu.dk>
2023-01-17 12:54:03 +0100

    Changing dice array to land on Hvidovrevej and the Roskildevej

--- src/test/java/SellControllerSequenceTests.java
+++ src/test/java/SellControllerSequenceTests.java
@@ -3,15 +3,14 @@ import GameComponents.LoadedCup;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 
-public class JailTest {
+public class SellControllerSequenceTests {
 
 
     public static void main(String[] args) {
 
         GameController game = new GameController();
-        LoadedCup cup = new LoadedCup(new ArrayList<>(Arrays.asList(new int[]{15, 15}, new int[]{2, 2})));
+        LoadedCup cup = new LoadedCup(new ArrayList<>(Arrays.asList(new int[]{1, 2}, new int[]{1, 2})));
         game.init();
         game.run(cup);
     }
